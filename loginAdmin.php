18c2fde19c39927481f28179ba711e97f5a87ecf
oussama7845 <abakarimoussama@gmail.com>
2023-01-05 23:11:05 +0000

    login

---
+++ loginAdmin.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="https://kit.fontawesome.com/b9398f24d6.js" crossorigin="anonymous"></script>
+    <title>Login page</title>
+
+</head>
+
+<body>
+    <header>
+        <div class="header_top">
+            <img src="assets/images/logo.png" alt="logo Harmonie">
+            <a href=""><i class="fa-sharp fa-solid fa-gear"></i></a>
+        </div>
+        <div class="header_txt">
+            <h2>Bienvenue sur l’application de L’Harmonie de Bergues</h2>
+        </div>
+    </header>
+    <hr>
+    <main>
+        <div class="main">
+            <h1>Connexion</h1>
+            <form action="" method="POST">
+                <div class="input">
+                    <input type="text" name="code" placeholder="Identifiant">
+                </div> <br>
+                <div class="input">
+                    <input type="text" name="code" placeholder="code d'accès">
+                </div>
+                <div class="button">
+                    <button>Se connecter</button>
+                </div>
+
+                <a href="">Probléme de <br> connexion ?</a>
+        </div>
+        </form>
+
+    </main>
+    <footer>
+        <p>Harmonie de Bergeues &#169; - 2022</p>
+    </footer>
+</body>
+
+</html>
---
+++ évènementdetails.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="https://kit.fontawesome.com/b9398f24d6.js" crossorigin="anonymous"></script>
+    <title>Login page</title>
+
+</head>
+
+<body>
+    
+    <?php include_once('assets/models/header.html') ?>
+       
+    
+    
+
+
+    <main>
+        <table>
+            <thead>
+                <tr>
+                    <th>Nom</th>
+                    <th>Prénom</th>
+                    <th>Téléphone</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>Abakarim</td>
+                    <td>Oussama</td>
+                    <td>+33 54 65 74 78</td>
+                    <td>dazjbdjaz@gmail.com</td>
+                </tr>
+            </tbody>
+        </table>
+    </main>
+    
+    <footer>
+        <p>Harmonie de Bergeues &#169; - 2022</p>
+    </footer>
+</body>
+
+</html>
