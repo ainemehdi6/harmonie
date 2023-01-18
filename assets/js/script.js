af967de5f6a260c87840c9d13d67da5b729ff13c
EL AINE El Mehdi <ainemehdi6@gmail.com>
2023-01-18 22:04:30 +0100

    Update event
    J'ai changé la methode de modification et j'ai integré le backEnd

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,14 @@
 
 function AddEventBoxOn() {
-	var popup = document.getElementById("job_post");
+	var popup = document.getElementById("add_post");
     var main = document.getElementById("main");
     var footer = document.getElementById("footer");
     popup.classList.toggle("active");
     main.classList.toggle("overlay");
 }
-function AddEventBoxOff() {
-	var popup = document.getElementById("job_post");
-    var main = document.getElementById("main");
-    popup.classList.toggle("active");
-    main.classList.remove("overlay");
-}
+
+
+
 
 
 
