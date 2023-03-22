6fbc0dfa353f0becdb0118348715581d715caedd
CMC59 <philippeplaia@gmail.com>
2023-03-22 15:37:21 +0100

    nav

--- assets/models/navbar.php
+++ assets/models/navbar.php
@@ -1,31 +1,37 @@
 
-<!-- Blue with white text -->
-<nav id="nav" class="navbar navbar-expand-sm bg-dark navbar-dark justify-content-center">
+<!-- Barre de navigation -->
+<nav id="nav" class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+    <!-- Marque -->
+    <a class="navbar-brand" href="#">Harmonie</a>
 
-  <!-- Links -->
-  <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link" href="UsersList.php">Gestion des membres</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="GestionEvents.php">Gestion des évènements</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="GestionAdmins.php">Gestion des Admins</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="ChangePassUser.php">Changer MDP User</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="ChangePassAdmin.php">Modifier MDP Admin</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="EditProfilAdmin.php">Modifier Profil Admin</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="controllers/logout.php">Déconnexion</a>
-    </li>
-  </ul>
-
+    <!-- Liens de navigation -->
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="UsersList.php">Gestion des membres</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="GestionEvents.php">Gestion des évènements</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="GestionAdmins.php">Gestion des Admins</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="ChangePassUser.php">Changer MDP User</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="ChangePassAdmin.php">Modifier MDP Admin</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="EditProfilAdmin.php">Modifier Profil Admin</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="controllers/logout.php">Déconnexion</a>
+        </li>
+      </ul>
+    </div>
+  </div>
 </nav>
-
